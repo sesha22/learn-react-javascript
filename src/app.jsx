66eda6679f8d032d81b82fb3c1efa44e03434dc3
Galuh Wasesa <surat.sesha@gmail.com>
2025-05-11 18:14:58 +0700

    chore : create react javascript with vite

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./button.jsx";
+
+export function App() {
+  return (
+    <div>
+      <h1>Learn React JavaScript</h1>
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
+}
